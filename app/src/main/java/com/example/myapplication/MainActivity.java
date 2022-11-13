f35fbfa6224fdb8f46b636b569be0a4299576ec3
Daviddeekk <81929651+Daviddeekk@users.noreply.github.com>
2022-11-13 21:10:24 +0100

    Učení se práce v AS. Přidáno proklikávání mezi layouty.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //access to activity_main
+
+
+    }
+    public void poStisknuti (View view){
+        //Button tlacitkoBeh = findViewById(R.id.beh);
+        //tlacitkoBeh.setText("Běhání");
+        setContentView(R.layout.hromadny);
+    }
+    public void zpet (View view)
+    {
         setContentView(R.layout.activity_main);
     }
 }
